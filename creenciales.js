2efcf7feaec2cb8755c069da0e0f41de4765d013
Marfer795 <fgomezcor795@gmail.com>
2024-10-24 14:01:01 -0500

    Suprimir Gitignore

--- creenciales.js
+++
@@ -1,3 +0,0 @@
-const CLIENT_ID = '440921674984-ckgaf27jvv61cvkfee5piuacrelfsn7g.apps.googleusercontent.com';
-const API_KEY = 'AIzaSyATLu1RE3__fu_IcfKnTG75_VBahnQ96c4';
-const sheetID = '1SmUuC2xFGzgYbuASx8wHKCZ5Gn9kdKUpOkbIN7syeI4';
